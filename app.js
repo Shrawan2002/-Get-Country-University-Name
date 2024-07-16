63277a0510d26beec14481426b6fa713b14f69c4
Shrawan2002 <shrawankumar1754@gmail.com>
2024-07-16 14:18:04 +0530

    initial files

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-let url = "https://universities.hipolabs.com/search?name=";
-let country = "Nepal"
+let url = "http://universities.hipolabs.com/search?name=";
 
-async function getCollege(){
+async function getCollege(country){
     let rep = await axios.get(url+country);
-    console.log(rep);
+    console.log(rep.data[0].name);
+    return rep.data;
 
+}
+
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", async ()=>{
+    let country = document.querySelector("input").value;
+    console.log(country);
+    let colArr = await getCollege(country);
+    show(colArr);
+})
+
+function show(colArr){
+    let list = document.querySelector(".list");
+    list.innerText = ""
+    for(cola of colArr){
+        console.log(cola.name);
+        let li = document.createElement("li");
+        li.innerText = cola.name;
+        list.appendChild(li);
+    }
 }
